549638d8199cea04fc1ac84c53dbedae1be0c433
anaschwartz <aname4ever@gmail.com>
2024-09-24 14:04:29 -0700

    image click

---
+++ class-exercises/dom-imagesClick/js/main.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector("#dogs").addEventListener("click", function(){
+    document.querySelector("#dogs").style.visibilty = "hidden";
+})
+
+document.querySelector("#noki_front").addEventListener("click", function(){
+    document.querySelector("#noki_front").style.visibilty = "hidden";
+})
+
+document.querySelector("#noki_side").addEventListener("click", function(){
+    document.querySelector("#noki_side").style.visibilty = "hidden";
+})
